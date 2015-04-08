5c5a5b38788bc68e595d7990686aaf0c67f5c29e
董铂然 <dantesx2012@gmail.com>
2015-04-08 23:03:01 +0800

    初始化项目
    初始化项目

---
+++ SXBaiduDoctor/SXBaiduDoctor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SXBaiduDoctor
+//
+//  Created by 董 尚先 on 15/4/8.
+//  Copyright (c) 2015年 shangxianDante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
